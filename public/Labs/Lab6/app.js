f02009e68ec9f5ec1db7223b65cb67349a73dd9f
TheMightyHoosier <ec2-user@ip-172-31-34-238.us-east-2.compute.internal>
2019-10-22 20:51:00 +0000

    in class work

---
+++ public/Labs/Lab6/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+app.engine("html", require('ejs').renderFile);
+app.use(express.static("public"));
+
+//routes
+app.get("/", function(req, res){
+    res.send("It works!");
+});
+
+app.get("/mercury", function(req, res){
+    res.send("This will be a Mercury web page!");
+});
+
+app.get("/venus", function(req, res){
+    res.send("This will be a Venus web page!");
+});
+
+//server listener
+app.listen("8080", "0.0.0.0", function(){
+    console.log("Express Server is Running...");
+});
+
+//Finished off on page 7
---
+++ public/exercises/node/includes/object.js
@@ -0,0 +1,4 @@
+var me = {
+    st: "string",
+    dt: Date
+};
